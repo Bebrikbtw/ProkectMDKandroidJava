8cbbe96e3359bb53f826ddbb6f60d3e9afd0a8c2
117-10 <23>
2023-02-28 12:43:05 +0300

    Практика 11 Java 28.02.23

---
+++ resvalues/src/main/java/com/example/resvalues/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.resvalues;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedinstanceState) {
+        super.onCreate(savedinstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
+
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.bntBottomText);
+    }
+}
