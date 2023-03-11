7ec960b30f3fcaa16280fd19603bc16d8d3080c1
Danil <123>
2023-03-11 14:50:51 +0300

    МДК практика 13 за 07.03.2023. Сделана полностью.

---
+++ contextmenu/src/main/java/com/example/contextmenu/Constants.java
@@ -0,0 +1,10 @@
+package com.example.contextmenu;
+
+public class Constants {
+    public static final int MENU_COLOR_RED = 1;
+    public static final int MENU_COLOR_GREEN = 2;
+    public static final int MENU_COLOR_BLUE = 3;
+    public static final int MENU_SIZE_22 = 4;
+    public static final int MENU_SIZE_26 = 5;
+    public static final int MENU_SIZE_30 = 6;
+}
