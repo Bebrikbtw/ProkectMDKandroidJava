73637d9e4a9be45d3cc3bb4af69d0e900b207698
Danil <123>
2023-04-06 16:08:25 +0300

    МДК практика 17 за 17.04.2023. Выполнена полностью.

--- animation/src/main/java/com/example/animation/MainActivity.java
+++ animation/src/main/java/com/example/animation/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.animation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-
+// Протестировано - полностью работает, все функции работают так, как нужно.
 public class MainActivity extends AppCompatActivity {
     final int MENU_ALPHA_ID = 1;
     final int MENU_SCALE_ID = 2;
@@ -14,15 +18,17 @@ public class MainActivity extends AppCompatActivity {
     final int MENU_ROTATE_ID = 4;
     final int MENU_COMBO_ID = 5;
     TextView tv;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = (TextView) findViewById(R.id.tv);
+        tv = findViewById(R.id.tv);
         registerForContextMenu(tv);
     }
+
     @Override
-        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         switch (v.getId()) {
             case R.id.tv:
                 menu.add(0, MENU_ALPHA_ID, 0, "alpha");
@@ -34,3 +40,28 @@ public class MainActivity extends AppCompatActivity {
         }
         super.onCreateContextMenu(menu, v, menuInfo);
     }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        Animation anim = null;
+        switch(item.getItemId()){
+            case MENU_ALPHA_ID:
+                anim = AnimationUtils.loadAnimation(this,R.anim.myalpha);
+                break;
+            case MENU_TRANSLATE_ID:
+                anim = AnimationUtils.loadAnimation(this,R.anim.mytrans);
+                break;
+            case MENU_COMBO_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.mycombo);
+                break;
+            case MENU_ROTATE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myrotate);
+                break;
+            case MENU_SCALE_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.myscale);
+                break;
+        }
+        tv.startAnimation(anim);
+        return super.onContextItemSelected(item);
+    }
+}
